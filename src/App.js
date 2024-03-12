24e61925b70df48b8195d0dff214abd21da86650
mrwinrock <mrwinrock11@gmail.com>
2024-03-12 09:11:23 +0700

    init commit react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="ahhh">
+        <p>hello</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
