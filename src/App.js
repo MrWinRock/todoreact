5057f9b7489e1f1cb49e1acb3d78ae4fa1ac42a9
mrwinrock <mrwinrock11@gmail.com>
2024-03-16 22:16:36 +0700

    popcat, bg add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
-import { TodoWrapper } from './components/TodoWrapper';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import "./App.css";
+import { TodoWrapper } from "./components/TodoWrapper";
+import Footer from "./components/Footer";
 
 function App() {
+  const [clicks, setClicks] = useState(null);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
     <div className="App">
-      <TodoWrapper  />
-      <Footer />
+      <div className="bg-image">
+      </div>
+        <div className="popcat" onClick={handleClick}>Click Me!</div>
+        <div className="clicked">{clicks}</div>
+        <TodoWrapper />
+        <Footer />
     </div>
   );
 }
