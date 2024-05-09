819e6040eb1b22bcc4136256575688d3abc864ce
MrWinRock <mrwinrock11@gmail.com>
2024-05-09 15:01:46 +0700

    change contributor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <h1>Contributor</h1>
-      <div className="contributors">
-        <div>001 อภิวิชญ์</div>
-        <div>025 เจษฎา</div>
-        <div>003 เอสเธอร์</div>
-        <div>031 ปฏิวัฒน์</div>
-        <div>009 จิรัฎฐ์</div>
-        <div>037 ทรรศนัย</div>
-        <div>010 นภาภรณ์</div>
-        <div></div>
-      </div>
+      <h1>MrWinRock</h1>
     </footer>
   );
 }
